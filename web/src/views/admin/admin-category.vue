f7113113db3e0ecd2cece628b8e60c8d50718a41
MuBenYou <1458839724@qq.com>
2022-09-15 15:31:00 +0800

    9.15 使用递归算法，将树形数据中，当前目标节点及其子孙节点设置成disabled解决编辑保存局重新编辑时，数据不更新的问题,为树形选择增加一个“无”选择，用来新增一级节点

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -134,6 +134,8 @@ export default defineComponent({
      **/
     const handleQuery = () => {
       loading.value = true;
+      //如果不清空现有数据，刘编辑保存重新加载数据后，再点编辑
+      level1.value=[];
       axios.get("/category/all").then((response) => {
         loading.value = false;
         const data = response.data;
