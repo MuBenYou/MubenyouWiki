5656ee4c84ea38e435af1cf4a85d51b779ee4295
MuBenYou <1458839724@qq.com>
2022-09-24 16:43:54 +0800

    9.24 集成redis，登录成功后将登录信息放入redis中

--- src/main/java/com/mby/wiki/controller/UserControl.java
+++ src/main/java/com/mby/wiki/controller/UserControl.java
@@ -1,5 +1,6 @@
 package com.mby.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.mby.wiki.req.UserLoginReq;
 import com.mby.wiki.req.UserQueryReq;
 import com.mby.wiki.req.UserResetPasswordReq;
@@ -9,18 +10,30 @@ import com.mby.wiki.resp.PageResp;
 import com.mby.wiki.resp.UserLoginResp;
 import com.mby.wiki.resp.UserQueryResp;
 import com.mby.wiki.service.UserService;
+import com.mby.wiki.utils.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 //@Controller  返回页面 @RestController 是返回字符串的
 @RestController  //@ResponseBody用来返回字符串或JSON对象 大多是JSON对象
 @RequestMapping("/user")
 public class UserControl {
+    private static final Logger LOG = LoggerFactory.getLogger(UserControl.class);
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @RequestMapping("/list")  //接口支持所有的请求方式
     public CommonResp list(@Valid UserQueryReq req){
@@ -62,6 +75,11 @@ public class UserControl {
                 req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token，{},并放入redis中",token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp),3600*24, TimeUnit.SECONDS);
         resp.setContent(userLoginResp);
         return resp;
     }
