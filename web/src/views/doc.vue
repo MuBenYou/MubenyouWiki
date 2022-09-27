6bee9bccd0ec7a64877503f864887bef1c72c75d
MuBenYou <1458839724@qq.com>
2022-09-27 15:55:43 +0800

    9.27 增加文档点赞功能

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -25,6 +25,11 @@
             <a-divider style="height: 2px; background-color: #9999cc"/>
           </div>
           <div class="wangeditor" :innerHTML="html"></div>
+          <div class="vote-div">
+            <a-button type="primary" shape="round" :size="'large'" @click="vote">
+              <template #icon><LikeOutlined /> &nbsp;点赞：{{doc.voteCount}} </template>
+            </a-button>
+          </div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -104,7 +109,17 @@ export default defineComponent({
         handleQueryContent(selectedKeys[0]);
       }
     };
-
+    // 点赞
+    const vote = () => {
+      axios.get('/doc/vote/' + doc.value.id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          doc.value.voteCount++;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
 
 
     onMounted(() => {
@@ -118,7 +133,7 @@ export default defineComponent({
       onSelect,
       defaultSelectedKeys,
       doc,
-
+      vote,
     }
   }
 });
