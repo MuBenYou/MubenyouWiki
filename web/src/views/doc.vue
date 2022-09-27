621bbe7cb0fe804e050d05ca1702ef826fc2819a
MuBenYou <1458839724@qq.com>
2022-09-27 15:30:11 +0800

    9.27 文档页面景示文档阅读数点赞数

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -16,6 +16,14 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div>
+            <h2>{{doc.name}}</h2>
+            <div>
+              <span>阅读数：{{doc.viewCount}}</span> &nbsp; &nbsp;
+              <span>点赞数：{{doc.voteCount}}</span>
+            </div>
+            <a-divider style="height: 2px; background-color: #9999cc"/>
+          </div>
           <div class="wangeditor" :innerHTML="html"></div>
         </a-col>
       </a-row>
@@ -40,6 +48,9 @@ export default defineComponent({
     const html = ref();
     const defaultSelectedKeys = ref();
     defaultSelectedKeys.value = [];
+
+    const doc = ref();
+    doc.value = {};
 
     const level1 = ref(); //一级文档树，children属性就是二级文档
     level1.value = [];
@@ -73,6 +84,8 @@ export default defineComponent({
           if(Tool.isNotEmpty(level1)){
             defaultSelectedKeys.value = [level1.value[0].id];
             handleQueryContent(level1.value[0].id);
+            //初始显示文档信息
+            doc.value = level1.value[0];
           }
         }else {
           message.error(data.message);
@@ -85,6 +98,9 @@ export default defineComponent({
     const onSelect = (selectedKeys: any,info: any) =>{
       console.log('selected',selectedKeys,info);
       if(Tool.isNotEmpty(selectedKeys)){
+        //选中某一个节点时，加载该节点的文档信息
+        doc.value = info.selectedNodes[0].props;
+        //加载内容
         handleQueryContent(selectedKeys[0]);
       }
     };
@@ -101,6 +117,7 @@ export default defineComponent({
       html,
       onSelect,
       defaultSelectedKeys,
+      doc,
 
     }
   }
