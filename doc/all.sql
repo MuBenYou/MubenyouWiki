f65eb433318a8a337e7cc6142a585fda0c0eb222
MuBenYou <1458839724@qq.com>
2022-09-29 16:40:26 +0800

    9.27 11-3 增加电子书快照表，生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -116,3 +116,16 @@ update ebook t1,(select ebook_id,
 set t1.doc_count = t2.doc_count, t1.view_count = t2.view_count,
     t1.vote_count = t2.vote_count
 where t1.id = t2.ebook_id;
+
+#电子书快照表
+drop table if exists `ebook_snapshot`;
+create table `ebook_snapshot` (
+    `id` bigint auto_increment not null comment 'id',
+    `ebook_id` bigint not null default  0 comment '电子书id',
+    `date` date not null comment '快照日期',
+    `view_count` int not null default 0 comment '阅读数',
+    `vote_count` int not null default 0 comment '点赞数',
+    `view_increase` int not null default 0 comment '阅读增长',
+    `vote_increase` int not null default 0 comment '点赞增长',
+    primary key (`id`)
+)engine=innodb default charset =utf8mb4 comment='电子书快照表';
